b8defe1d9d8417d8fb16713992ef11f4c4e919fd
Siddharth Gupta <g.sidd97@gmail.com>
2019-11-20 04:11:03 -0800

    Update phase2.R
    - moving average plots and data for Global active power.
    - moving average plots and data for Global Reactive power.
    - moving average plots and data for Voltage.
    - moving average plots and data for Global Intensity.

--- phase2.R
+++ phase2.R
@@ -3,12 +3,14 @@ library(TTR)
 
 ds <- data
 test <- na.omit(
-            read.csv('U:/cmpt318/test_data/test1.txt')
-          # read.csv('~/git/cmpt318-term-project/TrainData.txt')
+          #  read.csv('U:/cmpt318/test_data/test1.txt')
+          read.csv('~/git/cmpt318-term-project/test_data/test1.txt')
         );
 
+### APROACH 1
+
 ###########################################
-###########     CHARACTERISTIC 1 ##########
+###########   CHARACTERISTIC 1   ##########
 ###########################################
 
 # ## [TRAINING DATA] Weekly minimums for wednesday evenings for GAP, V and GI
@@ -49,17 +51,7 @@ month.name <- function(n) {
                                               ifelse(n==8, 'August', 
                                                      ifelse(n==9, 'September',
                                                             ifelse(n==10, 'October',
-                                                                   ifelse(n==11, 'November', 'December')
-                                                            )
-                                                    )
-                                              )
-                                      )
-                              )
-                        )
-                  )
-            )
-    )
-  )
+                                                                   ifelse(n==11, 'November', 'December')))))))))))
 }
 
 ## plot point-anomalies for each month GAP
@@ -79,19 +71,19 @@ for (i in 1:12) {
   layout(1)
   plot(
     m$Global_active_power, main=paste('Anomalies for',month.name(i), sep=' '), 
-    col='black', lwd=1, xlab='Days ->', xaxt='n', ylab='Global Active Power',
+    col='black', lwd=1, xlab=paste('Days in', month.name(i), sep=' '), xaxt='n', ylab='Global Active Power',
     panel.first=grid(NULL,NULL,lwd=1,col='gray'), pch=20
   )
   points(below.idx,below, col='red', pch=20, lwd=2)
   points(above.idx,above, col='chocolate2', pch=20, lwd=2)
-  abline(month.maxs$GAP[i],0,col='green',lwd=3)
-  abline(month.mins$GAP[i],0,col='green',lwd=3)
-  legend('topright',c('Above','Normal','Below'),cex=.8,col=c('chocolate2','black','red'),pch=c(20,20,20))
+  abline(month.maxs$GAP[i],0,col='blue',lwd=4)
+  abline(month.mins$GAP[i],0,col='blue',lwd=4)
+  legend('topleft', c('Above','Normal','Below'), cex=0.7, col=c('chocolate2','black','red'),pch=c(20,20,20))
 }
 
-###########################################
-###########     CHARACTERISTIC 2 ##########
-###########################################
+############################################
+###########   CHARACTERISTIC 2   ###########
+############################################
 
 z <- 3
 # The z score threshold
@@ -100,19 +92,87 @@ z <- 3
 
 ## get raw data
 move.GAP <- data.frame(Raw = test$Global_active_power)
+move.GRP <- data.frame(Raw = test$Global_reactive_power)
+move.V   <- data.frame(Raw = test$Voltage)
+move.GI  <- data.frame(Raw = test$Global_intensity)
 
 ## run the moving average
 move.GAP$Mean <- runMean(move.GAP$Raw, n=20)
+move.GRP$Mean <- runMean(move.GRP$Raw, n=20)
+move.V$Mean   <- runMean(move.V$Raw, n=20)
+move.GI$Mean  <- runMean(move.GI$Raw, n=20)
 
 ## run the moving SD
 move.GAP$SD <- runSD(move.GAP$Raw, n=20)
+move.GRP$SD <- runSD(move.GRP$Raw, n=20)
+move.V$SD <- runSD(move.V$Raw, n=20)
+move.GI$SD <- runSD(move.GI$Raw, n=20)
 
 ## find anomalies
-anomaly.GAP <- move.GAP$Raw[( (move.GAP$Raw < move.GAP$Mean - z*move.GAP$SD) | (move.GAP$Raw > move.GAP$Mean + z*move.GAP$SD) & !is.na(move.GAP$Mean) )]
+
+#### Global Active Power anomalies
+anomaly.GAP <- move.GAP$Raw[( ((move.GAP$Raw < move.GAP$Mean - z*move.GAP$SD) | (move.GAP$Raw > move.GAP$Mean + z*move.GAP$SD)) & !is.na(move.GAP$Mean) )]
 anomaly.GAP <- anomaly.GAP[!is.na(anomaly.GAP)]
 ## accumulate indices
-anomaly.GAP.idx <- which(move.GAP$Raw < move.GAP$Mean - z*move.GAP$SD | move.GAP$Raw > move.GAP$Mean + z*move.GAP$SD )
+anomaly.GAP.idx <- which((move.GAP$Raw < move.GAP$Mean - z*move.GAP$SD | move.GAP$Raw > move.GAP$Mean + z*move.GAP$SD) & !is.na(move.GAP$Mean) )
+
+#### Global Reactive Power anomalies
+anomaly.GRP <- move.GRP$Raw[( ((move.GRP$Raw < move.GRP$Mean - z*move.GRP$SD) | (move.GRP$Raw > move.GRP$Mean + z*move.GRP$SD)) & !is.na(move.GRP$Mean) )]
+anomaly.GRP <- anomaly.GRP[!is.na(anomaly.GRP)]
+## accumulate indices
+anomaly.GRP.idx <- which((move.GRP$Raw < move.GRP$Mean - z*move.GRP$SD | move.GRP$Raw > move.GRP$Mean + z*move.GRP$SD) & !is.na(move.GRP$Mean) )
+
+#### Voltage anomalies
+anomaly.V <- move.V$Raw[( ((move.V$Raw < move.V$Mean - z*move.V$SD) | (move.V$Raw > move.V$Mean + z*move.V$SD)) & !is.na(move.V$Mean) )]
+anomaly.V <- anomaly.V[!is.na(anomaly.V)]
+## accumulate indices
+anomaly.V.idx <- which((move.V$Raw < move.V$Mean - z*move.V$SD | move.V$Raw > move.V$Mean + z*move.V$SD) & !is.na(move.V$Mean) )
+
+#### Global Intensity anomalies
+anomaly.GI <- move.GI$Raw[( ((move.GI$Raw < move.GI$Mean - z*move.GI$SD) | (move.GI$Raw > move.GI$Mean + z*move.GI$SD)) & !is.na(move.GI$Mean) )]
+anomaly.GI <- anomaly.GI[!is.na(anomaly.GI)]
+## accumulate indices
+anomaly.GI.idx <- which((move.GI$Raw < move.GI$Mean - z*move.GI$SD | move.GI$Raw > move.GI$Mean + z*move.GI$SD) & !is.na(move.GI$Mean) )
+
 
 ## Plot them mofos
-plot(move.GAP$Raw)
-points(anomaly.GAP.idx, anomaly.GAP,col='red', lwd=2)
+#### GAP
+layout(1)
+plot(
+  move.GAP$Raw, main='Moving average: Global Active Power Anomalies', col='black', pch=20,
+  lwd=1, xlab='Time', xaxt='n', ylab='Global Active Power', 
+  panel.first = grid(NULL, NULL, lwd=1, col='gray')
+)
+points(anomaly.GAP.idx, anomaly.GAP, col='dodgerblue2', pch=20, lwd=1)
+legend('topleft', c('Anomalies','Normal'), cex=0.7, pch=c(20,20), col=c('dodgerblue2','black'))
+
+#### GRP
+layout(1)
+plot(
+  move.GRP$Raw, main='Moving average: Global Reactive Power Anomalies', col='black', pch=20,
+  lwd=1, xlab='Time', xaxt='n', ylab='Global Reactive Power', 
+  panel.first = grid(NULL, NULL, lwd=1, col='gray')
+)
+points(anomaly.GRP.idx, anomaly.GRP, col='red', pch=20, lwd=1)
+legend('topleft', c('Anomalies','Normal'), cex=0.7, pch=c(20,20), col=c('red','black'))
+
+#### V
+layout(1)
+plot(
+  move.V$Raw, main='Moving average: Voltage Anomalies', col='black', pch=20,
+  lwd=1, xlab='Time', xaxt='n', ylab='Voltage', 
+  panel.first = grid(NULL, NULL, lwd=1, col='gray')
+)
+points(anomaly.V.idx, anomaly.V, col='darkorange3', pch=20, lwd=1)
+legend('bottomleft', c('Anomalies','Normal'), cex=0.7, pch=c(20,20), col=c('darkorange3','black'))
+
+#### GI
+layout(1)
+plot(
+  move.GI$Raw, main='Moving average: Global Intensity Anomalies', col='black', pch=20,
+  lwd=1, xlab='Time', xaxt='n', ylab='Global Intensity', 
+  panel.first = grid(NULL, NULL, lwd=1, col='gray')
+)
+points(anomaly.GI.idx, anomaly.GI, col='mediumvioletred', pch=20, lwd=1)
+legend('topleft', c('Anomalies','Normal'), cex=0.7, pch=c(20,20), col=c('mediumvioletred','black'))
+
