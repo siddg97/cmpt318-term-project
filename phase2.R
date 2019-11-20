0651e14bc4bbe7e3b70750f8838abbbb7475e78f
Siddharth Gupta <g.sidd97@gmail.com>
2019-11-20 01:35:48 -0800

    Phase 2
    + Added legend to plots for out of range
    + Improved color scheme for plots of out of range
    + Added some functionality for moving average

--- phase2.R
+++ phase2.R
@@ -2,7 +2,10 @@ library(lubridate)
 library(TTR)
 
 ds <- data
-test <- na.omit(read.csv('~/git/cmpt318-term-project/TrainData.txt'));
+test <- na.omit(
+            read.csv('U:/cmpt318/test_data/test1.txt')
+          # read.csv('~/git/cmpt318-term-project/TrainData.txt')
+        );
 
 ###########################################
 ###########     CHARACTERISTIC 1 ##########
@@ -35,28 +38,56 @@ month.maxs <- aggregate(
                   by= list(Month = month(as.Date(ds$Date, format='%d/%m/%Y'))), 
                   FUN = max
               )
+month.name <- function(n) {
+  ifelse(n==1,'January',
+    ifelse(n==2, 'Feburary', 
+           ifelse(n==3,'March', 
+                  ifelse(n==4, 'April', 
+                         ifelse(n==5, 'May', 
+                                ifelse(n==6, 'June', 
+                                       ifelse(n==7, 'July', 
+                                              ifelse(n==8, 'August', 
+                                                     ifelse(n==9, 'September',
+                                                            ifelse(n==10, 'October',
+                                                                   ifelse(n==11, 'November', 'December')
+                                                            )
+                                                    )
+                                              )
+                                      )
+                              )
+                        )
+                  )
+            )
+    )
+  )
+}
 
 ## plot point-anomalies for each month GAP
 for (i in 1:12) {
+  ## Get raw month data
   m <- test[month(test$Date)==i, ]
   
-  ## Find points below and their indices
+  ## Find points below min and their indices
   below <- m$Global_active_power[m$Global_active_power < month.mins$GAP[i]]
   below.idx <- which(m$Global_active_power < month.mins$GAP[i])
   
-  ## Find points above and their indices
+  ## Find points above max and their indices
   above <- m$Global_active_power[m$Global_active_power > month.maxs$GAP[i]]
   above.idx <- which(m$Global_active_power > month.maxs$GAP[i])
   
   ## Plot them mofos
   layout(1)
-  plot(m$Global_active_power, main=as.character(i))
-  points(below.idx,below, col='red', lwd=2)
-  points(above.idx,above, col='red', lwd=2)
-  abline(month.maxs$GAP[i],0,col='blue',lwd=2)
-  abline(month.mins$GAP[i],0,col='blue',lwd=2)
+  plot(
+    m$Global_active_power, main=paste('Anomalies for',month.name(i), sep=' '), 
+    col='black', lwd=1, xlab='Days ->', xaxt='n', ylab='Global Active Power',
+    panel.first=grid(NULL,NULL,lwd=1,col='gray'), pch=20
+  )
+  points(below.idx,below, col='red', pch=20, lwd=2)
+  points(above.idx,above, col='chocolate2', pch=20, lwd=2)
+  abline(month.maxs$GAP[i],0,col='green',lwd=3)
+  abline(month.mins$GAP[i],0,col='green',lwd=3)
+  legend(x=0,y=0,c('Above','Normal','Below'),cex=.8,col=c('chocolate2','black','red'),pch=c(20,20,20))
 }
-
 
 ###########################################
 ###########     CHARACTERISTIC 2 ##########
